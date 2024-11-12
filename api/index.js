d68a2107a00b1b86acf67ad131d3ed93e6a1f11b
Diane Hipolito <134621147+DianeHipolito@users.noreply.github.com>
2024-11-12 14:01:57 +0000

    decoy11

--- api/index.js
+++ api/index.js
@@ -36,7 +36,11 @@ app.get('/', (request, response) => {
             h1 {
               font-size: 3rem;
               margin-bottom: 10px;
+            }
+
+            h1:hover{
               text-shadow: 5px 5px 20px gray;
+              transition: 0.5;
             }
   
             h4 {
