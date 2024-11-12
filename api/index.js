f1e7e8c164cbd567a5a8897a576e2bcb967fcb91
Diane Hipolito <134621147+DianeHipolito@users.noreply.github.com>
2024-11-12 13:45:43 +0000

    decoy7

--- api/index.js
+++ api/index.js
@@ -11,7 +11,49 @@ app.listen(PORT, () => {
 
 app.get('/', (request, response) => {
     //response.status(200).json({ message: 'API Homepage' });
-    response.send('<h1>API Homepage</h1>');
+    response.send(`<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>API Homepage</title>
+          <!-- Google Fonts - Roboto -->
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+          <style>
+            body {
+              font-family: 'Roboto', sans-serif;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              height: 90vh;
+              margin: 0;
+              background-color: #f4f4f4;
+            }
+  
+            .content {
+              text-align: center;
+              color: #333;
+            }
+  
+            h1 {
+              font-size: 3rem;
+              margin-bottom: 10px;
+            }
+  
+            h4 {
+              font-size: 1.5rem;
+              font-weight: 400;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="content">
+            <h1>API Homepage</h1>
+            <h4>Hipolito, Diana Lei E.</h4>
+          </div>
+        </body>
+      </html>
+    `);
 });
 
 const customerRouter = require("./routes/customers");
